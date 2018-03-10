ae7b0234910a0fda1ad53be75b6a41bda1848cec
John L Papa <john@johnpapa.net>
2018-03-10 11:19:05 -0500

    minor tweaks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'heroes' },
@@ -11,11 +10,6 @@ const routes: Routes = [
   {
     path: 'villains',
     loadChildren: 'app/villains/villains.module#VillainsModule'
-  },
-  {
-    path: '**',
-    pathMatch: 'full',
-    component: PathNotFoundComponent
   }
 ];
 
