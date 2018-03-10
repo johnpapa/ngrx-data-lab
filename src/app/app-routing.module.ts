234170135390c03e0af7689c7d1e5c4e632ade95
John L Papa <john@johnpapa.net>
2018-03-10 11:13:10 -0500

    removing stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'heroes' },
@@ -10,6 +11,11 @@ const routes: Routes = [
   {
     path: 'villains',
     loadChildren: 'app/villains/villains.module#VillainsModule'
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: PathNotFoundComponent
   }
 ];
 
