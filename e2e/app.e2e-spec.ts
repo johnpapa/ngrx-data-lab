9555650706d4ce523f15b26366f234f7e8b10620
John L Papa <john@johnpapa.net>
2018-03-12 18:05:02 -0400

    e2e is working

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+'use strict'; // necessary for es6 output in node.js
 import { AppPage } from './app.po';
 
 describe('ngrx-data-lab App', () => {
@@ -7,8 +8,41 @@ describe('ngrx-data-lab App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  describe('Heroes', () => {
+    beforeEach(() => {
+      page.navigateToHeroes();
+    });
+    runAllComponentTests('heroes');
   });
+
+  describe('Villains', () => {
+    beforeEach(() => {
+      page.navigateToVillains();
+    });
+    runAllComponentTests('villains');
+  });
+
+  function runAllComponentTests(entityName: string) {
+    it(`should navigate to ${entityName}`, () => {
+      const link = page.getActiveLink();
+      expect(link.isPresent()).toBe(true);
+      expect(page.getListTitle().getText()).toMatch(
+        new RegExp(entityName, 'i')
+      );
+    });
+
+    it(`should have ${entityName} items > 0`, () => {
+      expect(page.getListItems().count()).toBeGreaterThan(0);
+    });
+
+    it(`should open detail when item is selected`, () => {
+      page
+        .getListItems()
+        .first()
+        .click()
+        .then(item =>
+          expect(page.getDetailTitle().getText()).toMatch('Details')
+        );
+    });
+  }
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,39 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateToHeroes() {
+    return browser.get('/heroes');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  navigateToVillains() {
+    return browser.get('/villains');
+  }
+
+  getToolbarLinks() {
+    return element.all(by.css('mat-toolbar a[routerlinkactive]'));
+  }
+
+  getActiveLink() {
+    return element(by.css('mat-toolbar a.router-link-active'));
+  }
+
+  getListTitle() {
+    return element(by.css('.list-container mat-card-title'));
+  }
+
+  getDetailTitle() {
+    return element(by.css('.detail-container mat-card-title'));
+  }
+
+  getListItems() {
+    return element.all(by.css('mat-card-content li'));
+  }
+
+  getTopButtons() {
+    const buttons = element.all(by.css('control-panel .button-panel button'));
+    return {
+      refresh: buttons[0],
+      add: buttons[1]
+    };
   }
 }
