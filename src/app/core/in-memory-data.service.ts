4d75366d26ae59f3dadbb15c686a1b9071ba868b
John L Papa <john@johnpapa.net>
2018-03-19 20:50:40 -0400

    if the api root is not === api, then let it pass through

--- src/app/core/in-memory-data.service.ts
+++ src/app/core/in-memory-data.service.ts
@@ -65,9 +65,11 @@ export class InMemoryDataService {
    */
   parseRequestUrl(url: string, utils: RequestInfoUtilities): ParsedRequestUrl {
     const parsed = utils.parseRequestUrl(url);
-    parsed.collectionName = this.active
-      ? mapCollectionName(parsed.collectionName)
-      : undefined;
+    const isDefaultRoot = parsed.apiBase === 'api';
+    parsed.collectionName =
+      this.active && isDefaultRoot
+        ? mapCollectionName(parsed.collectionName)
+        : undefined;
     return parsed;
   }
 }
@@ -137,7 +139,7 @@ function getDbData() {
     {
       id: 25,
       name: 'West Witch',
-      saying: "I'll get you, my pretty, and your little dog too!"
+      saying: `I'll get you, my pretty, and your little dog too!`
     },
     {
       id: 26,
